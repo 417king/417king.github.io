57b86174fe24390ef0d59cb64bd598c461bd0baf
417king <167139053+417king@users.noreply.github.com>
2025-07-27 19:59:34 -0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function toggleSwitch(el) {
+    el.classList.toggle('active');
+    document.body.classList.toggle('dark-mode');
+}
+
+function animateOnLoad() {
+    document.querySelector('.new-header').classList.add('animate-in');
+    document.querySelector('.hero').classList.add('animate-in');
+    document.querySelector('.hero-box').classList.add('animate-in');
+}
